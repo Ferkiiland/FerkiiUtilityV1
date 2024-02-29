bb8f81534ca484ed9aa0709e799f58a4f3e8e3fe
Ferkiiland <113061637+Ferkiiland@users.noreply.github.com>
2024-02-29 16:09:05 -0500

    Update fix.bat

--- fix.bat
+++ fix.bat
@@ -1,5 +1,6 @@
 @echo off
 RMDIR /Q/S C:\fxr
+RMDIR /Q/S C:\ProgramData\XitFerkiito.bat
 RMDIR /Q/S C:\Users\%username%\AppData\Roaming\Microsoft\Windows\'Start Menu'\Programs\Startup\starup.vbs
 Reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "Shell" /t REG_SZ /d "explorer.exe" /f
 powershell Start-Process explorer.exe
